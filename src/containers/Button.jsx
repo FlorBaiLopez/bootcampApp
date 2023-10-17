47bf6557ac623a1e19dd545099689ce0e54545e7
FlorBaiLopez <florenciambl18@gmail.com>
2023-10-17 13:41:25 -0300

    avances practico 4 y 6

---
+++ src/containers/Button.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+
+
+const boton = {
+    name: PropTypes.string, 
+    type: PropTypes.string, 
+    isDisabled: PropTypes.bool, 
+    onClick: PropTypes.func
+};
+
+
+
+
---
+++ src/containers/Movie.jsx
@@ -0,0 +1,14 @@
+
+const peli1 = {
+    title: 'Nowhere',
+    releaseYear: 2023,
+    image: () => {
+        let img = new Image();
+        img.src = 'https://cdn-0.comoacaba.com/wp-content/uploads/2023/10/rpzFxv78UvYG5yQba2soO5mMl4T.jpg?ezimgfmt=rs:352x528/rscb5/ngcb5/notWebP';
+        return img;
+    }
+}
+
+
+
+const peliculas = []
