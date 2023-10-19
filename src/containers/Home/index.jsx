37071e127441bf37959fa3756ac03d8e1c4aa00b
FlorBaiLopez <florenciambl18@gmail.com>
2023-10-19 17:27:03 -0300

    avances practico 5 Api con Lia

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import MovieList from 'Components/MovieList';
 import moviesData from 'Data/movies';
 
+import getMovies from '../../api/movies';
+
 import './styles.scss';
 
-const Home = () => (
-  <div className="home">
-    <MovieList movies={moviesData} />
-  </div>
-);
+const Home = () => {
+  //const [movies, setMovies] = useState([]);
+  const initHome = async () => {
+    const movies = await getMovies();
+    // hacer traid catch 
+
+    console.log(movies);
+  };
+
+  useEffect(() => {
+    initHome();
+  }, []);
+
+  return (
+    <div className="home">
+      <MovieList movies={moviesData} />
+    </div>
+  );
+};
 
 export default Home;
